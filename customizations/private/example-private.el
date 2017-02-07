1915347b76e94ae75c8ea1e0f9dd67d94050e93c
ProbablyJody <jody@housealford.com>
2017-02-07 14:03:28 -0600

    Added private customizations functionality

---
+++ customizations/private/example-private.el
@@ -0,0 +1,4 @@
+;; Customizations for Work
+(add-to-list 'load-path "~/.emacs.d/customizations/private/my-employer")
+(load "employer-project.el")
+
--- init.el
+++ init.el
@@ -139,6 +139,10 @@
 (load "setup-js.el")
 (load "ruby.el")
 
+;; Customizations not tracked by version control
+(add-to-list 'load-path "~/.emacs.d/customizations/private")
+(load "private.el")
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
